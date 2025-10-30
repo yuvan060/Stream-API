42c92464a04ccad533f25c57d77c5dcf8effdb32
yuvan060 <yuvansankar060@gmail.com>
2025-10-30 21:23:31 +0530

    added map functionality

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,11 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list = List.of(1,3,5,7,9,2,4,6,8,10,12,214,16,18,20);
         Stream<Integer> stream = new Stream<>(list);
-        Stream<Integer> stream1 = stream.filter(i->i>3)
-                .filter(i->i%2 == 0);
+        Stream<String> stream1 = stream.filter(i->i>3)
+                .filter(i->i%2 == 0)
+                .map(i->String.valueOf(i));
         System.out.println("Performing lazy evaluation...");
         stream1.forEach(System.out::println);
-        stream.forEach(System.out::println);
+//        stream.forEach(System.out::println);
     }
 }
--- src/com/stream/api/Stream.java
+++ src/com/stream/api/Stream.java
@@ -1,52 +1,62 @@
-package com.stream.api;
+    package com.stream.api;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+    import java.util.ArrayList;
+    import java.util.List;
+    import java.util.function.Consumer;
+    import java.util.function.Function;
+    import java.util.function.Predicate;
+    import java.lang.RuntimeException;
 
-public class Stream<T> {
-    private final List<T> source;
-    private final List<Function<Object, Object>> operations;
-    private boolean isConsumed;
+    public class Stream<T> {
+        private final List<T> source;
+        private final List<Function<Object, Object>> operations;
+        private Stream<?> downStream;
 
-    public Stream(List<T> source) {
-        this(source, new ArrayList<>());
-    }
-
-    public Stream(List<T> source, List<Function<Object, Object>> operations) {
-        this.source = source;
-        this.operations = operations;
-        this.isConsumed = false;
-    }
-
-    public Stream<T> filter(Predicate<T> predicate) {
-        if(isConsumed) {
-            throw new RuntimeException("Streams cannot be consumed again");
+        public Stream(List<T> source) {
+            this(source, new ArrayList<>());
         }
-        this.isConsumed = true;
-        List<Function<Object, Object>> newOperations =  operations;
-        Function<Object, Object> predicateObj = (item) -> predicate.test((T)item) ? item : null;
-        newOperations.add(predicateObj);
-        return new Stream<>(source, newOperations);
-    }
 
-    public void forEach(Consumer<Object> consumer) {
-        if(isConsumed) {
-            throw new RuntimeException("Streams cannot be consumed again");
+        public Stream(List<T> source, List<Function<Object, Object>> operations) {
+            this.source = source;
+            this.operations = operations;
         }
-        this.isConsumed = true;
-        for(T i : source) {
-            Object current = i;
-            for(Function<Object, Object> opr : this.operations) {
-                current = opr.apply(current);
-                if(current == null) {
-                    break;
+
+        public Stream<T> filter(Predicate<T> predicate) {
+            isConsumed();
+            List<Function<Object, Object>> newOperations =  new ArrayList<>(operations);
+            Function<Object, Object> predicateObj = (item) -> predicate.test((T)item) ? item : null;
+            newOperations.add(predicateObj);
+            this.downStream =  new Stream<>(source, newOperations);
+            return (Stream<T>) this.downStream;
+        }
+
+        public <R> Stream<R> map(Function<T, R> function) {
+            isConsumed();
+            List<Function<Object, Object>> newOperations = new ArrayList<>(operations);
+            Function<Object, Object> funObj = (item) -> function.apply((T)item);
+            newOperations.add(funObj);
+            this.downStream = new Stream<>(source, newOperations);
+            return (Stream<R>) this.downStream;
+        }
+
+        public void forEach(Consumer<Object> consumer) {
+            isConsumed();
+            for(T i : source) {
+                Object current = i;
+                for(Function<Object, Object> opr : this.operations) {
+                    current = opr.apply(current);
+                    if(current == null) {
+                        break;
+                    }
                 }
+                if(current != null)
+                    consumer.accept(current);
             }
-            if(current != null)
-                consumer.accept(current);
+        }
+
+        private void isConsumed() {
+            if(this.downStream != null) {
+                throw new RuntimeException("Stream can not be consumed again...");
+            }
         }
     }
-}
